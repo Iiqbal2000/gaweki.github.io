a02b698fcbabba31e91d3f7962ce31acb6dbf1d4
andreltokdis <andrel.karunia.s@mail.ugm.ac.id>
2020-09-17 21:46:35 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a9d3b1203aec8d00e924.js"
+    "url": "webpack-runtime-448da1af46d54bf0a2ea.js"
   },
   {
     "url": "framework-376edee25eb5f5cd8260.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5a729ec27da7e03e53e90aaad6a3b6bc"
+    "revision": "90169677f393924a45272518e92fd226"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
